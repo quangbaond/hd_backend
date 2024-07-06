0dbe66bf04251e2c14be84e17c8483cd9b4de9fd
quangbaond <quangbaorp@gmail.com>
2024-07-07 00:43:47 +0700

    develop

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-var uri = process.env.MONGODB_URI;
+var uri = process.env.MONGODB_URI || "mongodb://userAdmin:baooibao1@202.92.6.135:27017/hd";
 
 mongoose.connect(uri);
 
